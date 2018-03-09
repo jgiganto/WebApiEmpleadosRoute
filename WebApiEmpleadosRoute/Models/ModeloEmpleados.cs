f2d43fb22dc7ffe5d284d4213ea52a54357f97aa
jgiganto <jgiganto1@hotmail.com>
2018-03-09 10:37:24 +0100

    s

---
+++ WebApiEmpleadosRoute/Models/ModeloEmpleados.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiEmpleadosRoute.Models
+{
+    public class ModeloEmpleados
+    {
+        HOSPITALEntities contexto;
+        public ModeloEmpleados()
+        {
+            contexto = new HOSPITALEntities();
+        }
+
+        public List<EMP> GetEmpleados()
+        {
+            var consulta = from datos in contexto.EMP
+                           select datos;
+            return consulta.ToList();
+        }
+        public EMP BuscarEmpleado(int empno)
+        {
+            return contexto.EMP.Find(empno);
+        }
+        public List<EMP> BuscarEmpleadosSalario(int salario)
+        {
+            var consulta = from datos in contexto.EMP
+                           where datos.SALARIO >= salario
+                           select datos;
+            if(consulta.Count()== 0)
+            {
+                return null;
+            }
+            else
+            {
+                return consulta.ToList();
+            }
+        }
+    }
+}
